d26040ba3c57b2e90f23f7616ed3d8a5dea221dc
Ralf Henschkowski <ralf.henschkowski@swisscom.com>
2025-03-10 17:53:09 +0100

    Add python version to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,10 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Framework :: Django",
     ],
 )
